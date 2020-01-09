1eb0227d6097c7f3be82ba5c9e6431f4a9735eb8
aq17 <aqneco@gmail.com>
2020-01-09 21:54:02 +0900

    3.2.1 グローバルコンポーネント

--- Leaning-Vue-2/Chapter3/app.js
+++ Leaning-Vue-2/Chapter3/app.js
@@ -1,7 +1,38 @@
-Vue.component('list-item', {
-  template: '<li>foo</li>'
+Vue.component('fruits-list-title', {
+  template: '<h1>フルーツ一覧</h1>'
 })
 
+Vue.component('fruits-list-description', {
+  template: '<p>季節の代表的なフルーツの一覧です</p>'
+})
+
+Vue.component('fruits-list-table', {
+  template: `
+  <table>
+    <tr>
+      <th>季節</th>
+      <th>フルーツ</th>
+    </tr>
+    <tr>
+      <td>春</td>
+      <td>いちご</td>
+    </tr>
+    <tr>
+      <td>夏</td>
+      <td>スイカ</td>
+    </tr>
+    <tr>
+      <td>秋</td>
+      <td>ぶどう</td>
+    </tr>
+    <tr>
+      <td>冬</td>
+      <td>みかん</td>
+    </tr>
+  </table>
+  `
+})
+ 
 new Vue({
-  el: '#example'
+  el: '#fruits-list'
 })
