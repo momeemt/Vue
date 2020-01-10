9c224e3b38e5166d8e5e0475e7f4ffe803470e7e
aq17 <aqneco@gmail.com>
2020-01-10 23:44:27 +0900

    4.4.2 APIによるデータ通信

--- Leaning-Vue-2/Chapter4/app.js
+++ Leaning-Vue-2/Chapter4/app.js
@@ -1,5 +1,46 @@
+let getUsers = function (callback) {
+  setTimeout(function () {
+    callback(null, [
+      {
+        id: 1,
+        name: 'Takuya Tejima'
+      },
+      {
+        id: 2,
+        name: 'Yohei Noda'
+      }
+    ])
+  }, 1000)
+}
+
 let UserList = {
-  template: '#user-list'
+  template: '#user-list',
+  data: function () {
+    return {
+      loading: false,
+      users: function () { return [] },
+      error: null
+    }
+  },
+  created: function () {
+    this.fetchData()
+  },
+  watch: {
+    '$route': 'fetchData'
+  },
+  methods: {
+    fetchData: function () {
+      this.loading = true
+      getUsers((function (err, users) {
+        this.loading= false
+        if (err) {
+          this.error = err.tostring()
+        } else {
+          this.users = users
+        }
+      }).bind(this))
+    }
+  }
 }
 
 let router = new VueRouter({
