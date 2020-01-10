3fbec5208076a86d797ee0104ac10ebc364449a2
aq17 <aqneco@gmail.com>
2020-01-11 06:57:33 +0900

    4.4.3 詳細ページの実装

--- Leaning-Vue-2/Chapter4/app.js
+++ Leaning-Vue-2/Chapter4/app.js
@@ -1,3 +1,55 @@
+let userData = [
+  {
+    id: 1,
+    name: 'Takuya Tejima',
+    description: '東南アジアで働くエンジニアです。'
+  },
+  {
+    id: 2,
+    name: 'Yohei Noda',
+    description: 'アウトドア・フットサルが趣味のエンジニアです。'
+  }
+]
+
+let UserDetail = {
+  template: '#user-detail',
+  data: function () {
+    return {
+      loading: false,
+      user: null,
+      error: null
+    }
+  },
+  created: function () {
+    this.fetchData()
+  },
+  watch: {
+    '$route': 'fetchData'
+  },
+  methods: {
+    fetchData: function () {
+      this.loading = true
+      getUser(this.$route.params.userId, (function (err, user) {
+        this.loading = false
+        if (err) {
+          this.error = err.tostring()
+        } else {
+          this.user = user
+        }
+      }).bind(this))
+    }
+  }
+}
+
+let getUser = function (userId, callback) {
+  setTimeout(function () {
+    let filteredUsers = userData.filter(function (user) {
+      return user.id === parseInt(userId, 10)
+    })
+    callback(null, filteredUsers && filteredUsers[0])
+  }, 1000)
+}
+
 let getUsers = function (callback) {
   setTimeout(function () {
     callback(null, [
@@ -54,6 +106,10 @@ let router = new VueRouter({
     {
       path: '/users',
       component: UserList
+    },
+    {
+      path: '/users/:userId',
+      component: UserDetail
     }
   ]
 })
