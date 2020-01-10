387487ee586fa5a9bce12af4623ded737793faf4
aq17 <aqneco@gmail.com>
2020-01-10 22:39:36 +0900

    3.3.2 子コンポーネントから親コンポーネントへの通信

--- Leaning-Vue-2/Chapter3/app.js
+++ Leaning-Vue-2/Chapter3/app.js
@@ -1,3 +1,37 @@
+let counterButton = Vue.extend({
+  template: '<span>{{counter}}個<button v-on:click="addToCart">追加</button></span>',
+  data: function() {
+    return {
+      counter: 0
+    }
+  },
+  methods: {
+    addToCart: function () {
+      this.counter += 1
+      this.$emit('increment')
+    }
+  },
+})
+
+const fruits_counter_object = new Vue({
+  el: '#fruits-counter',
+  components: {
+    'counter-button': counterButton
+  },
+  data: {
+    total: 0,
+    fruits: [
+      {name: '梨'},
+      {name: 'イチゴ'}
+    ]
+  },
+  methods: {
+    incrementCartStatus: function () {
+      this.total += 1
+    }
+  }
+})
+
 Vue.component('item-desc', {
   props: {
     itemName: {
