aaf17a61a6951808a210bd619743cb97c27e9a77
aq17 <aqneco@gmail.com>
2020-01-11 15:16:30 +0900

    4.4.4 ユーザー登録ページの実装

--- Leaning-Vue-2/Chapter4/app.js
+++ Leaning-Vue-2/Chapter4/app.js
@@ -1,3 +1,53 @@
+let postUser = function (params, callback) {
+  setTimeout(function () {
+    params.id = userData.length + 1
+    userData.push(params)
+    callback(null, params)
+  }, 1000)
+}
+
+let UserCreate = {
+  template: '#user-create',
+  data: function () {
+    return {
+      sending: false,
+      user: this.defaultUser(),
+      error: null
+    }
+  },
+  created: function () {
+  },
+  methods: {
+    defaultUser: function () {
+      return {
+        name: '',
+        description: ''
+      }
+    },
+    createUser: function () {
+      if (this.user.name.trim() === '') {
+        this.error = 'Nameは必須です'
+        return
+      }
+      if (this.user.description.trim() === '') {
+        this.error = 'Descriptionは必須です'
+        return
+      }
+      postUser(this.user, (function (err, user) {
+        this.sending = false
+        if (err) {
+          this.error = err.toString()
+        } else {
+          this.error = null
+          this.user = this.defaultUser()
+          alert('新規ユーザーが登録されました')
+          this.$router.push('/users')
+        }
+      }).bind(this))
+    }
+  }
+}
+
 let userData = [
   {
     id: 1,
@@ -106,6 +156,10 @@ let router = new VueRouter({
     {
       path: '/users',
       component: UserList
+    },
+    {
+      path: '/users/new',
+      component: UserCreate
     },
     {
       path: '/users/:userId',
